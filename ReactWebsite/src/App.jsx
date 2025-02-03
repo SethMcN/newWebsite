ce15d3f2bdaf374c4bfc093f93624b5a12bb572d
SethMcn <107069935+SethMcN@users.noreply.github.com>
2025-02-03 23:09:55 +0000

    initialize React project with Vite, Tailwind CSS, new react website incoming

---
+++ ReactWebsite/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="border-4 border-red-500">
+      <h1 className="mainHeader">Hello, World!</h1>
+      <h1 className="mainHeader">My name is seth </h1>
+      <h1 className="mainHeader">:)</h1>
+    </div>
+  );
+}
+
+export default App;
